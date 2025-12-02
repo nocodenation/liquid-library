bbe43af1d982022ea72dc5a3d2eb7974a0707f35
nocodenation <166212288+nocodenation@users.noreply.github.com>
2025-12-02 14:21:29 +0100

    Update src/native_python_processors/GoogleOAuthManager/GoogleOAuthManager.py
    Co-authored-by: Timur Izmagambetov <timur+github@iztiev.dev>

--- src/native_python_processors/GoogleOAuthManager/GoogleOAuthManager.py
+++ src/native_python_processors/GoogleOAuthManager/GoogleOAuthManager.py
@@ -24,7 +24,7 @@ class GoogleOAuthManager(FlowFileTransform):
         2. EXCHANGE_CODE: Exchanges the authorization code (received in callback) for access/refresh tokens.
         """
         tags = ['google', 'oauth', 'auth', 'token', 'helper']
-        dependencies = ['google-auth-oauthlib', 'requests']
+        dependencies = ['google-auth-oauthlib==1.2.3', 'requests==2.32.5']
 
     def __init__(self, **kwargs):
         pass
