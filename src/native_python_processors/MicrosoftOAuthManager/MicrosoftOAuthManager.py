349b812a24bf19042b3f702790bd8c291ca5279f
nocodenation <166212288+nocodenation@users.noreply.github.com>
2025-12-02 14:21:55 +0100

    Update src/native_python_processors/MicrosoftOAuthManager/MicrosoftOAuthManager.py
    Co-authored-by: Timur Izmagambetov <timur+github@iztiev.dev>

--- src/native_python_processors/MicrosoftOAuthManager/MicrosoftOAuthManager.py
+++ src/native_python_processors/MicrosoftOAuthManager/MicrosoftOAuthManager.py
@@ -24,7 +24,7 @@ class MicrosoftOAuthManager(FlowFileTransform):
         suitable for use by the GetMicrosoftMail processor.
         """
         tags = ['microsoft', 'graph', 'oauth', 'auth', 'token', 'helper']
-        dependencies = ['msal', 'requests']
+        dependencies = ['msal==1.34.0', 'requests==2.32.5']
 
     def __init__(self, **kwargs):
         pass
