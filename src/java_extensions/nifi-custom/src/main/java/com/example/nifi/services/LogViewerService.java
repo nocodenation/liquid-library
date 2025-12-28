d38a6233e618fd79df5de759328114ef2a74de81
Timur Izmagambetov <iztiev@gmail.com>
2025-12-28 14:55:10 +0500

    Revert "Merge pull request #11 from nocodenation/chore/update-flow-definitions"
    This reverts commit ef5435ea1fd9106714b043255fdbb6ccb1feb479, reversing
    changes made to c21293a72929cf3d70168a84b442ce663cf795ea.

--- src/java_extensions/nifi-custom/src/main/java/com/example/nifi/services/LogViewerService.java
+++
@@ -1,7 +0,0 @@
-package com.example.nifi.services;
-
-import org.apache.nifi.controller.ControllerService;
-
-public interface LogViewerService extends ControllerService {
-    // Interface marker
-}
